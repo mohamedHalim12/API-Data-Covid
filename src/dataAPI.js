6006da05ee60ab648266cf6e8a0b36236aca98b8
mohamedHalim12 <mohamedhalim.ambdourrahmane@uit.ac.ma>
2021-09-13 20:39:57 +0100

    first commit: all data are available and clear, next time, implementation of the DOM

---
+++ src/dataAPI.js
@@ -0,0 +1,33 @@
+(async () => {
+  const datalink = await getData("https://covid19.mathdro.id/api");
+  const { confirmed } = datalink;
+  const data = await getData(confirmed.detail);
+  // console.log(data);
+
+  const filterData = ({ provinceState, countryRegion, confirmed, deaths }) => ({
+    provinceState,
+    countryRegion,
+    confirmed,
+    deaths,
+  });
+  const sortData = ({ countryRegion: a }, { countryRegion: b }) => {
+    if (a > b) return 1;
+    else return -1;
+  };
+  const datafinal = data.map(filterData).sort(sortData);
+  console.log(datafinal);
+})();
+
+async function getData(url) {
+  try {
+    const res = await fetch(url);
+    if (!res.ok) {
+      throw new Error("Erreur not found");
+    }
+    const dataGlobale = await res.json();
+    // console.log(dataGlobale);
+    return dataGlobale;
+  } catch (err) {
+    console.error(err.message);
+  }
+}
